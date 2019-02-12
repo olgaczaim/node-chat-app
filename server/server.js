9c500b29460b31f52ed9c386bb5dd71d12b6e7c3
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-12 16:30:42 +0300

    chat app startup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 const express = require('express');
 
 var app = express();
 
-app.use(e)
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
