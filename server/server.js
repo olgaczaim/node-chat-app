76d17ae45a5acfc019c93bcbe457e1f62d629df5
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-15 16:12:43 +0300

    Great new user and alert others

--- server/server.js
+++ server/server.js
@@ -20,13 +20,30 @@ io.on('connection', (socket) => {
     //    createdAt: 123123
     //});
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from:'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        //io.emit('newMessage', {
+        //    from: message.from,
+        //    text: message.text,
+        //    createdAt: new Date().getTime()
+        //})
+        socket.broadcast.emit('newMessage', {
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
