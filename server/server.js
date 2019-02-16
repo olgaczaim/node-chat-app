5714318b8dacfb417e598f34a76c791e836b95b9
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-16 12:36:33 +0300

    message
    add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,24 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    //socket.emit('newMessage', {
-    //    from: 'John',
-    //    text: 'See you then',
-    //    createdAt: 123123
-    //});
-
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        //io.emit('newMessage', {
-        //    from: message.from,
-        //    text: message.text,
-        //    createdAt: new Date().getTime()
-        //})
-        socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
+        // socket.broadcast.emit('newMessage', {
+        //   from: message.from,
+        //   text: message.text,
+        //   createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
