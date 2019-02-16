02e7dca43c09acdca18a92892862a21d2d7f33e9
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-16 12:58:19 +0300

    grab location

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,19 @@ jQuery('#message-form').on('submit', function (e) {
 
     });
 });
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+   if(!navigator.geolocation){
+       return alert('Geolocation not supported by yout browser');
+   }
+   navigator.geolocation.getCurrentPosition(function (position) {
+       socket.emit('createLocationMessage', {
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude
+       });
+   }, function (e) {
+       alert('Unable to fetch location.')
+   })
+});
